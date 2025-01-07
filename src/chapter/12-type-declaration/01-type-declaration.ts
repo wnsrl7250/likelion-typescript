48d511157573c8e2ca490a89fc3d129304c3f997
wnsrl7250 <wnsrl7250@naver.com>
2025-01-07 16:02:54 +0900

    [learn] TypeScript 타입 선언 파일 (*.d.ts)

--- src/chapter/12-type-declaration/01-type-declaration.ts
+++ src/chapter/12-type-declaration/01-type-declaration.ts
@@ -7,7 +7,10 @@
 // - TypeScript는 선언된 타입 파일을 읽어 사용자에게 올바르게 작성하는 방법을 안내합니다.
 // ------------------------------------------------------------------------------
 
-import _ from 'lodash';
+import _ from "lodash";
+import axios, { AxiosResponse } from "axios";
+
+console.log(_.shuffle([2, 3, 4, 5, 6]));
 
 // axios 라이브러리를 설치한 후, 라이브러리에 선언된 타입 파일(index.d.ts)을 참고합니다.
 // TypeScript는 설치된 라이브러리 package.json 파일 types 또는 typing 항목에 연결된 타입 선언 파일을 확인합니다.
@@ -15,10 +18,39 @@ import _ from 'lodash';
 
 {
   const API = {
-    users: 'https://jsonplaceholder.typicode.com/users',
-    posts: 'https://jsonplaceholder.typicode.com/posts',
+    users: "https://jsonplaceholder.typicode.com/users",
+    posts: "https://jsonplaceholder.typicode.com/posts",
   };
 
   // axios를 사용해 사용자 또는 포스트 데이터를 비동기 요청/응답 받도록 합니다.
   // axios 요청 시, 전달받을 데이터 타입을 정의하는 방법을 살펴봅니다.
+  const response = await axios.get<{}, AxiosResponse<User>, {}>(API.users);
+
+  console.log(response.data.username);
+
+  const dummyUser = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  };
+
+  type User = typeof dummyUser;
 }
