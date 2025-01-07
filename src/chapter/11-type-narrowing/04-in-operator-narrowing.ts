57656ef8a352a66ff915e7a4ff630e54324ed597
wnsrl7250 <wnsrl7250@naver.com>
2025-01-07 16:01:45 +0900

    [learn] in 연산자 타입 범위 축소

--- src/chapter/11-type-narrowing/04-in-operator-narrowing.ts
+++ src/chapter/11-type-narrowing/04-in-operator-narrowing.ts
@@ -6,6 +6,9 @@
 // - 객체 작업 전에 객체 타입에 따라 특정 값이 포함되었는지 확인할 때 사용할 수 있습니다.
 // --------------------------------------------------------------------------
 
+// JavaScript에서 브라우저가 CSS Grid를 지원하는지 확인하는 방법?
+// 'grid' in document.body.style
+
 {
   type Cat = {
     name: string;
@@ -20,14 +23,14 @@
   const talk = (creature: Cat | Dog) => {
     console.log(creature.name);
     // 크리쳐(creature)가 가진 속성 포함 여부 확인을 위한 타입 범위 축소를 작성합니다.
-    creature.meow();
-    creature.bark();
+    if ("meow" in creature) creature.meow();
+    if ("bark" in creature) creature.bark();
   };
 
   const kitty: Cat = {
-    name: '키티',
+    name: "키티",
     meow() {
-      console.log('냐옹~');
+      console.log("냐옹~");
     },
   };
 
@@ -48,17 +51,22 @@
 
   // 무비, TV 드라마 타입 별 상영시간을 문제 없이 반환하도록 타입 범위를 축소합니다.
   function getRuntime(media: Movie | TV_Drama): number | void {
-    return media.episodeDuration * media.numberOfEpisodes;
-    return media.duration;
+    if ("episodeDuration" in media && "numberOfEpisodes" in media) {
+      return media.episodeDuration * media.numberOfEpisodes;
+    }
+
+    if ("duration" in media) {
+      return media.duration;
+    }
   }
 
   getRuntime({
-    title: '시간을 거스르는 남자',
+    title: "시간을 거스르는 남자",
     duration: 62,
   });
 
   getRuntime({
-    title: '우리 마을 이야기',
+    title: "우리 마을 이야기",
     numberOfEpisodes: 12,
     episodeDuration: 48,
   });
